a0a79eccd5ff8224b319a8a3b22df0659020bf4b
Abdullah Zeidan <abdullahzeidan@hotmail.com>
2022-12-05 16:09:06 +0200

    implement conversion functions still need to implement 'start()' function
    OTHER CHANGES:
    - add 📦 enquirer & readdirp and replace to ttf2woff2-no-gyp

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-async function start() {
-  console.log('hello start.')
+const Enquirer = require('enquirer')
+const fs = require('fs/promises')
+const readdirp = require('readdirp')
+const ttf2woff2 = require('ttf2woff2-no-gyp')
+
+async function start(args) {
+  if (args.length < 1) {
+  }
+  args.forEach(arg => {
+    if (isADir(arg)) {
+      convertToWoff2Recursively(arg)
+    } else if (isAFile(arg)) {
+      convertFileToWoff2(arg)
+    }
+  })
 }
 
-start()
+async function convertFileToWoff2(file) {
+  try {
+    const pathWithoutExtension = file.fullPath.substring(
+      0,
+      file.fullPath.lastIndexOf('.ttf')
+    )
+
+    const newFilePath = pathWithoutExtension + '.woff2'
+    const oldFileData = await fs.readFile(file.fullPath)
+
+    return fs.writeFile(newFilePath, ttf2woff2(oldFileData))
+  } catch (err) {
+    console.log(
+      '😢 conversion of',
+      file.basename,
+      'to woff2 failed:',
+      err.message
+    )
+  }
+}
+
+async function convertToWoff2Recursively(dir) {
+  for await (const entry of readdirp(dir)) {
+    if (entry.path.endsWith('.ttf')) {
+      await convertFileToWoff2(entry)
+    }
+  }
+  console.log(`🎉 conversion successful for files in '${dir}'`)
+}
+
+start(process.argv.slice(2))
+console.log(process.argv.slice(1))
+console.log(process.argv.slice(2))
